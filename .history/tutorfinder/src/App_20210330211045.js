1974f3f2db3e95312134b56d42ac51aa36902477
Dav1497 <david.carrion@upr.edu>
2021-03-30 23:55:45 -0400

    remade signup box

---
+++ .history/tutorfinder/src/App_20210330211045.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import TutorInfo from './components/TutorInfo'
+import './App.css';
+import Home from './Home';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import BulletinBoard from './BulletinBoard';
+import Nav from './Nav.js';
+import Welcome from "./Welcome";
+
+function App() {
+  return (
+    <div className="App">
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
+          integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l"
+          crossorigin="anonymous"
+        />
+      </head>
+      {/* <Home></Home> */}
+      <TutorInfo></TutorInfo>
+        {/* <Navbar fixed="top" />
+        <TutorInfo /> */}
+        {/* <BulletinBoard></BulletinBoard> */}
+        
+    </div>
+  );
+}
+
+
+
+export default App;
---
+++ .history/tutorfinder/src/Login_20210330204157.js
@@ -0,0 +1,66 @@
+import React from "react";
+import "./App.css";
+import "./Welcome.css"
+import { Input, FormGroup, Label, Form, Container, Button, Col } from 'reactstrap';
+
+class Login extends React.Component {
+
+  render() {
+
+    return (
+      <div className="login">
+        <h1 className="">Login</h1>
+        <div className="fullWidth color">
+          <Container>
+            <Form>
+              <br>
+              </br>
+              <Col className="">
+                <FormGroup inline>
+                  <div className="text-left">
+                    <Label >Email</Label>
+                  </div>
+                  <br></br>
+                  <Input
+                    type="email"
+                    name="email"
+
+                    id="exampleEmail"
+                    placeholder="email"
+                    className="size"
+                  />
+                </FormGroup>
+              </Col>
+              <br></br>
+              <Col className="">
+                <FormGroup >
+                  <div className="text-left">
+                    <Label for="">Password</Label>
+                  </div>
+                  <br></br>
+                  <Input
+                    type="password"
+                    name="password"
+
+                    id="examplePassword"
+                    placeholder="contraseña"
+                    className="size"
+                  />
+                </FormGroup>
+              </Col>
+              <div className="text-right">
+                <br></br>
+
+                <Button color="danger" type="submit" className="buttons submit"  >Submit</Button>
+              </div>
+            </Form>
+            <br>
+            </br>
+          </Container>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Login;
