7c464b94b20216d1ab300aefce99b1f112506c22
Christopher Vegerano <christopher.vegerano@upr.edu>
2021-04-03 00:57:43 -0400

    Implemented all tables except tutors.

---
+++ server/database/migrations/1617423755677_certifications.ts
@@ -0,0 +1,23 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class Certifications extends BaseSchema {
+  protected tableName = 'certifications'
+
+  public async up () {
+    let exist = await this.schema.hasTable(this.tableName)
+ 
+    if(!exist) {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('cid')
+      table.string('cschool')
+      table.string('csubject')
+      table.string('cserial_name')
+      table.timestamps(true)
+    })
+  }
+}
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
---
+++ server/database/migrations/1617424782741_level_of_educations.ts
@@ -0,0 +1,20 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class LevelOfEducations extends BaseSchema {
+  protected tableName = 'level_of_educations'
+
+  public async up () {
+    let exist = await this.schema.hasTable(this.tableName)
+ 
+    if(!exist) {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('leid')
+      table.string('lename')
+      table.timestamps(true)
+    })
+  }
+  }
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
