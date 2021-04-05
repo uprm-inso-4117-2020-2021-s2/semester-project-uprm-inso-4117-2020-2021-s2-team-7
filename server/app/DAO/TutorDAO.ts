500addf2266c75e185c90704f8be87baf2d130d5
everod52 <er30406@gmail.com>
2021-04-04 20:00:58 -0400

    commit to deploy

--- server/app/DAO/TutorDAO.ts
+++ server/app/DAO/TutorDAO.ts
@@ -1,6 +1,7 @@
 import Tutor from 'App/Models/Tutor'
 
 class TutorDAO implements BaseDAO<Tutor> {
+  // Create Tutor.
   public async create(params: Object): Promise<Tutor> {
     return await Tutor.create(params)
   }
