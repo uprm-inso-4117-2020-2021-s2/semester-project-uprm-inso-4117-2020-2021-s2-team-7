4cdb153f57e0d164f99235a0af31e505ef24c558
estefaniatc <estefania.torres1@upr.edu>
2021-05-03 11:00:30 -0400

    filtering for subjects

--- tutorfinder/src/BulletinBoard.js
+++ tutorfinder/src/BulletinBoard.js
@@ -14,7 +14,8 @@ class BulletinBoard extends React.Component {
             tutors: [],
             subjectOptions: [],
             educOptions: [],
-            changes: false
+            changes: false,
+            filteredTutors: []
         }
     }
 
@@ -28,7 +29,7 @@ class BulletinBoard extends React.Component {
         await axios.get(SERVER_URL + 'tutors').then(res => {
             console.log(res);
             this.setState({
-                tutors: res.data
+                tutors: res.data,
             })
         })
     }
@@ -43,12 +44,12 @@ class BulletinBoard extends React.Component {
                 }
                 this.state.subjectOptions.push(new_obj);
             });
-            this.setState({changes: true});
+            this.setState({ changes: true });
         });
     }
 
     async getEducOptions() {
-        await axios.get(SERVER_URL +'levelOfEducations').then(res => {
+        await axios.get(SERVER_URL + 'levelOfEducations').then(res => {
             console.log(res);
             res.data.map(educ => {
                 const new_educ = {
@@ -64,7 +65,7 @@ class BulletinBoard extends React.Component {
     }
 
     getSubjectArrByTutor(tutor) {
-        const subArr=[]
+        const subArr = []
         tutor.offers.map(off => {
             subArr.push(off.subject_id);
         })
@@ -73,45 +74,106 @@ class BulletinBoard extends React.Component {
     }
 
     firstRowTutors() {
-        return (
-            this.state.tutors.map((tutor, i) => (
-                (i < 3) &&
-                <TutorBox
-                    tutorId={tutor?.tid}
-                    firstName={tutor?.t_first_name}
-                    lastName={tutor?.t_last_name}
-                    nationality={tutor?.t_nationality}
-                    overview={tutor?.t_overview}
-                    subjectArr={this.getSubjectArrByTutor(tutor)}
-                />
-            ))
-        );
+        console.log(this.state.filteredTutors)
+        if (this.state.filteredTutors.length > 0) {
+            return (
+                this.state.filteredTutors.map((tutor, i) => (
+                    (i < 3) &&
+                    <TutorBox
+                        tutorId={tutor?.tid}
+                        firstName={tutor?.t_first_name}
+                        lastName={tutor?.t_last_name}
+                        nationality={tutor?.t_nationality}
+                        overview={tutor?.t_overview}
+                        subjectArr={this.getSubjectArrByTutor(tutor)}
+                    />
+                ))
+            );
+        } else {
+            return (
+                this.state.tutors.map((tutor, i) => (
+                    (i < 3) &&
+                    <TutorBox
+                        tutorId={tutor?.tid}
+                        firstName={tutor?.t_first_name}
+                        lastName={tutor?.t_last_name}
+                        nationality={tutor?.t_nationality}
+                        overview={tutor?.t_overview}
+                        subjectArr={this.getSubjectArrByTutor(tutor)}
+                    />
+                ))
+            );
+        }
     }
 
     secRowTutors() {
-        return (
-            this.state.tutors.map((tutor, i) => (
-                (i >= 3) && (i < 6) &&
+
+        if (this.state.filteredTutors.length > 0) {
+            return (
+                this.state.filteredTutors.map((tutor, i) => (
+                    (i >= 3) && (i < 6) &&
                     <TutorBox
-                    tutorId={tutor?.tid}
-                    firstName={tutor?.t_first_name}
-                    lastName={tutor?.t_last_name}
-                    nationality={tutor?.t_nationality}
-                    overview={tutor?.t_overview}
-                    subjectArr={this.getSubjectArrByTutor(tutor)}
+                        tutorId={tutor?.tid}
+                        firstName={tutor?.t_first_name}
+                        lastName={tutor?.t_last_name}
+                        nationality={tutor?.t_nationality}
+                        overview={tutor?.t_overview}
+                        subjectArr={this.getSubjectArrByTutor(tutor)}
                     />
-            ))
-        );
+                ))
+            );
+        } else {
+            return (
+                this.state.tutors.map((tutor, i) => (
+                    (i >= 3) && (i < 6) &&
+                    <TutorBox
+                        tutorId={tutor?.tid}
+                        firstName={tutor?.t_first_name}
+                        lastName={tutor?.t_last_name}
+                        nationality={tutor?.t_nationality}
+                        overview={tutor?.t_overview}
+                        subjectArr={this.getSubjectArrByTutor(tutor)}
+                    />
+                ))
+            );
+        }
+    }
+
+    handleSubjectOptionChange(e) {
+        // e.preventDefault();
+        let temp = [];
+        console.log(e);
+        this.state.tutors.map(tut => {
+            let hasSubject = false;
+            tut.offers.map(off => {
+                if (e.value == off.subject_id) {
+                    hasSubject = true;
+                }
+            });
+            if (hasSubject) {
+                temp.push(tut);
+            }
+        });
+        if (e.value) {
+            this.setState({
+                filteredTutors: temp
+            })
+        } else {
+            this.setState({
+                filteredTutors: []
+            })
+        }
+        console.log(this.state.filteredTutors);
     }
 
     render() {
         return (
             <div className="mainBg">
                 <div className="optionsBg">
-                    <Select className="dropdownStyle" options={this.state.subjectOptions} placeholder="Subjects" isMulti />
+                    <Select className="dropdownStyle" options={this.state.subjectOptions} placeholder="Subjects" onChange={this.handleSubjectOptionChange.bind(this)} />
                     <Select className="dropdownStyle" options={this.state.educOptions} placeholder="Level of Education" isMulti />
-                    <button className="pageButton">Previous</button>
-                    <button className="pageButton">Next</button>
+                    {/* <button className="pageButton">Previous</button>
+                    <button className="pageButton">Next</button> */}
                 </div>
                 <div className="boardBg">
                     <div style={{ display: "flex", flexDirection: "row" }}>
